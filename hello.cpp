082e0ca3e01b0e378ab7ac91c15e9594d8df8b33
samvaio <samvaiotmp@gmail.com>
2019-03-17 00:17:37 -0500

    scope for namespace

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int a = 10;
+
+namespace space {
+    int a = 20;
+}
+
 int main(int argc, char const *argv[])
 {
-    cout << "Hello World !!!";
+    cout << "Hello World !!!"<<"\n";
+    cout << "global a: " << a << "\n";
+    cout << "space a: " << space::a << "\n";
     return 0;
 }
